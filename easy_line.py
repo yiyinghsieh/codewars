176cf0d946c6524a599a5927f25f1e18b5772e8b
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-03-05 23:03:02 +0100

    Add easy_line.py

---
+++ easy_line.py
@@ -0,0 +1,77 @@
+"""Easy Line
+7 kyu
+
+URL: https://www.codewars.com/kata/56e7d40129035aed6c000632/train/python
+
+In the drawing below we have a part of the Pascal's triangle, lines are 
+numbered from zero (top).
+
+[     [1],
+     [1, 1],
+    [1, 2, 1],
+  [1, 3, 3, 1],
+ [1, 4, 6, 4, 1],
+ ...
+]
+
+We want to calculate the sum of the squares of the binomial coefficients 
+on a given line with a function called easyline (or easyLine or easy-line).
+Can you write a program which calculate easyline(n) where n is the line 
+number?
+The function will take n (with: n>= 0) as parameter and will return the 
+sum of the squares of the binomial coefficients on line n.
+
+##Examples:
+easyline(0) => 1
+easyline(1) => 2
+easyline(4) => 70
+easyline(50) => 100891344545564193334812497256
+
+##Ref: http://mathworld.wolfram.com/BinomialCoefficient.html
+
+Note:
+In Javascript, Coffeescript, Typescript, C++, PHP, C, R, Nim, Fortran to 
+get around the fact that we have no big integers the function easyLine(n) 
+will in fact return
+
+round(log(easyline(n)))
+and not the easyline(n) of the other languages.
+So, in Javascript, Coffeescript, Typescript, C++, PHP, R, Nim, C, Fortran:
+
+easyLine(0) => 0
+easyLine(1) => 1
+easyLine(4) => 4
+easyLine(50) => 67
+"""
+
+
+def easyline(n):
+
+
+def main():
+
+
+# def testing(actual, expected):
+#     Test.assert_equals(actual, expected)
+
+# Test.describe("easyline")
+# Test.it("Basic tests")
+# testing(easyline(7), 3432)
+# testing(easyline(13), 10400600)
+# testing(easyline(17), 2333606220)
+# testing(easyline(19), 35345263800)
+
+
+if __name__ == '__main__':
+	main()
+
+	
+
+
+
+
+
+
+
+
+
