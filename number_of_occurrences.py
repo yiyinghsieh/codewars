87708c2d38036ec833171ce1e9f88a07295f6ba5
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-03-12 23:43:21 +0100

    Add number_of_occurrences.py

---
+++ number_of_occurrences.py
@@ -0,0 +1,31 @@
+"""Number Of Occurrences
+7 kyu
+
+URL: https:https://www.codewars.com/kata/52829c5fe08baf7edc00122b/train/python
+
+Write a functionthat returns the number of occurrences of an element in an array.
+
+Examples
+sample = [0, 1, 2, 2, 3]
+number_of_occurrences(0, sample) == 1
+number_of_occurrences(4, sample) == 0
+number_of_occurrences(2, sample) == 2
+number_of_occurrences(3, sample) == 1
+"""
+
+
+def number_of_occurrences(element, sample):
+
+
+def main():
+
+
+sample = [0, 1, 2, 2, 3]
+test.assert_equals(number_of_occurrences(0, sample), 1)
+test.assert_equals(number_of_occurrences(4, sample), 0)
+test.assert_equals(number_of_occurrences(2, sample), 2)
+test.assert_equals(number_of_occurrences(3, sample), 1)
+
+
+if __name__ == '__main__':
+	main()
