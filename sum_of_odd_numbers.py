c8eb0620d12b3372d192bc2eee948ca3544ea444
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-03-14 22:55:45 +0100

    Complete sum_of_odd_numbers.py

--- sum_of_odd_numbers.py
+++ sum_of_odd_numbers.py
@@ -18,26 +18,39 @@ row_sum_odd_numbers(2); # 3 + 5 = 8
 """
 
 
-def row_sum_odd_numbers(n):
+def row_sum_odd_numbers1(n):
+    # Edge case.
+    if n == 1:
+        return 1
 
+    n_odds = sum([i for i in range(1, n + 1)])
+
+    idx = 1
+    odd = 1
+    lst = []
+    while idx != n_odds:
+        odd += 2
+        lst.append(odd)
+        idx += 1
+
+    return sum(lst[-n:])
 
 
 def main():
-	# Output: 1
-	n = 1
-	print(row_sum_odd_numbers(n))
+    # # Output: 1
+    # n = 1
+    # print(row_sum_odd_numbers(n))
 
-
-Test.assert_equals(row_sum_odd_numbers(1), 1)
-Test.assert_equals(row_sum_odd_numbers(2), 8)
-Test.assert_equals(row_sum_odd_numbers(13), 2197)
-Test.assert_equals(row_sum_odd_numbers(19), 6859)
-Test.assert_equals(row_sum_odd_numbers(41), 68921)
+    assert row_sum_odd_numbers1(1) == 1
+    assert row_sum_odd_numbers1(2) == 8
+    assert row_sum_odd_numbers1(13) == 2197
+    assert row_sum_odd_numbers1(19) == 6859
+    assert row_sum_odd_numbers1(41) == 68921
 
 
 if __name__ == '__main__':
-	main()
+    main()
 
-	
+
 
 
