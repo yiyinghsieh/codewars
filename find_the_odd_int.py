719995040df9a4c4ff39d029f892ce4a6ef64cd1
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-03-16 19:04:42 +0100

    Complete find_it

--- find_the_odd_int.py
+++ find_the_odd_int.py
@@ -10,26 +10,50 @@ times.
 """
 
 
-def find_it(seq):
-    dic = dict()
-    for seqs in seq:
-        dic[seqs] = dic.get(seqs, 0) + 1
-    for k, v in dic.items():
+def find_it1(seq):
+    # Create a dict:num->count.
+    num_count_d = dict()
+    for num in seq:
+        num_count_d[num] = num_count_d.get(num, 0) + 1
+
+    for k, v in num_count_d.items():
         if v % 2 != 0:
             return k
     
+def find_it1(seq):
+    from collections import defaultdict
+    # Create a dict:num->count.
+    num_count_d = defaultdict(int)  # default count = 0
+    for num in seq:
+        num_count_d[num] += 1
+
+    for k, v in num_count_d.items():
+        if v % 2 != 0:
+            return k
+
+
+def find_it2(seq):
+    return [i for i in seq if seq.count(i) % 2][0] # only one odd num
+    
 
 def main():
-    # # Output: 5
-    # seq = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
-    # print(find_it(seq))
+    # Output: 5
+    seq = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
+    print(find_it1(seq))
 
-    assert find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) == 5
-    assert find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) == -1
-    assert find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) == 5
-    assert find_it([10]) == 10
-    assert find_it([1,1,1,1,1,1,10,1,1,1,1]) == 10
-    assert find_it([5,4,3,2,1,5,4,3,2,10,10]) == 1
+    assert find_it1([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) == 5
+    assert find_it1([1,1,2,-2,5,2,4,4,-1,-2,5]) == -1
+    assert find_it1([20,1,1,2,2,3,3,5,5,4,20,4,5]) == 5
+    assert find_it1([10]) == 10
+    assert find_it1([1,1,1,1,1,1,10,1,1,1,1]) == 10
+    assert find_it1([5,4,3,2,1,5,4,3,2,10,10]) == 1
+
+    assert find_it2([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) == 5
+    assert find_it2([1,1,2,-2,5,2,4,4,-1,-2,5]) == -1
+    assert find_it2([20,1,1,2,2,3,3,5,5,4,20,4,5]) == 5
+    assert find_it2([10]) == 10
+    assert find_it2([1,1,1,1,1,1,10,1,1,1,1]) == 10
+    assert find_it2([5,4,3,2,1,5,4,3,2,10,10]) == 1
 
 
 if __name__ == '__main__':
