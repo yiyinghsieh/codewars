8f158461afc80fae712c48fd7f041123acaa9674
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-03-31 19:43:33 +0200

    Complete array_diff.py

--- array_diff.py
+++ array_diff.py
@@ -3,26 +3,29 @@
 
 URL:https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
 
-Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-
+Your goal in this kata is to implement a difference function, which 
+subtracts one list from another and returns the result.
 It should remove all values from list a, which are present in list b.
 
 array_diff([1,2],[1]) == [2]
-If a value is present in b, all of its occurrences must be removed from the other:
-
+If a value is present in b, all of its occurrences must be removed from 
+the other:
 array_diff([1,2,2,2,3],[2]) == [1,3]
 """
 
 
 def array_diff(a, b):
-
-
+    result = []
+    for i in a:
+        if i not in b:
+            result.append(i)
+    return result  
+    
 
 def main():
-	# Output: [2]
-	a, b = [1,2], [1]
-	print(array_diff(a, b))
-
+    # # Output: []
+    # a, b = [], [1,2]
+    # print(array_diff(a, b))
 
  assert array_diff([1,2], [1]) == [2], "a was [1,2], b was [1], expected [2]"
  assert array_diff([1,2,2], [1]) == [2,2], "a was [1,2,2], b was [1], expected [2,2]"
@@ -31,6 +34,6 @@ def main():
  assert array_diff([], [1,2]) == [], "a was [], b was [1,2], expected []"
 
 
+if __name__ == '__main__':
+    main()
 
-if __main__ == '__name__':
-	main()
