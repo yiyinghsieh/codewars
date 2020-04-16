52d92e8f73de405135485eb2c1519787e1bc80b7
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-04-16 22:48:56 +0200

    Complete break_camelcase.py

--- break_camelcase.py
+++ break_camelcase.py
@@ -12,22 +12,32 @@ solution("camelCasing")  ==  "camel Casing"
 
 
 def solution(s):
+    import string
+    n = len(s)
+    lst = []
 
-
-
-
-
+    uppercase = set(string.ascii_uppercase)
+    for i in range(n):
+        if s[i] in uppercase:
+            lst.append(' ')
+        lst.append(s[i])
+    return ''.join(lst)
 
 
 def main():
-	Output: "hello World" 
-	s = "helloWorld"
-	print(solution(s))
+    # # Output: "hello World" 
+    # s = "helloWorld"
+    # print(solution(s))
 
-	assert solution("helloWorld") == "hello World"
-	assert solution("camelCase") == "camel Case"
-	assert solution("breakCamelCase") == "break Camel Case"
+    # # Output: "break Camel Case" 
+    # s = "breakCamelCase"
+    # print(solution(s))
+
+    assert solution("helloWorld") == "hello World"
+    assert solution("camelCase") == "camel Case"
+    assert solution("breakCamelCase") == "break Camel Case"
 
 
 if __name__ == '__main__':
-	main()
+    main()
+
