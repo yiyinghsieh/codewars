d215d86b9ac19a57b36b91b9647ef41d91fcbf59
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-04-22 21:22:38 +0200

    Complete backwards_read_primes.py

--- backwards_read_primes.py
+++ backwards_read_primes.py
@@ -31,42 +31,64 @@ backwardsPrime(2, 100) => [13, 17, 31, 37, 71, 73, 79, 97]
 backwardsPrime(9900, 10000) => [9923, 9931, 9941, 9967]
 """
 
+
 # backwardsPrime is renamed backwards_prime
 def backwards_prime(start, stop):
-    
     lst = []
-    lst_no = []
-    while start < stop:
+    if start < 4:
+        start = 4
+    while start < stop + 1:
         for i in range(2, int(start ** 0.5 + 1)):
             if start % i == 0:
                 break
         if start % i != 0:
             lst.append(start)
         start += 1
-    # print(lst)
+    
     lst1 = []
     for i in lst:
-        # print(str(i)[::-1])
         ii = str(i)[::-1]
-        # print(ii)
+        if int(ii) != i:
+            lst1.append(int(ii))
 
-    
-    
+    lst2 = []
+    if stop <= 100:
+        for i in lst1:
+            if i > 11 and i in lst:
+                i_i = str(i)[::-1]
+                lst2.append(int(i_i))
+        return lst2
+    else:
+        for i in lst1:
+            for ii in range(2, int(i ** 0.5 + 1)):
+                if i % ii == 0:
+                    break
+            if i % ii != 0:
+                i_i = str(i)[::-1]
+                lst2.append(int(i_i))
+        return lst2
+
 
 def main():
-    # Output: [7027, 7043, 7057]
-    # start, stop = 7000, 7100
-    start, stop = 4, 100
+    # Output: [13, 17, 31, 37, 71, 73, 79, 97]
+    start, stop = 2, 100
     print(backwards_prime(start, stop))
 
-    # # Output: []
-    # start, stop = 400, 503
-    # print(backwards_prime(start, stop))
+    # Output: [9923, 9931, 9941, 9967]
+    start, stop = 9900, 10000
+    print(backwards_prime(start, stop))
 
-     # a1 = [7027, 7043, 7057]
-     # testing(7000, 7100, a1)  
-     # a8 = []
-     # testing(400, 503, a8)
+    # Output: []
+    start, stop = 501, 599
+    print(backwards_prime(start, stop))
+
+    # Output: []
+    start, stop = 400, 503
+    print(backwards_prime(start, stop))
+
+    # Output: [7027, 7043, 7057]
+    start, stop = 7000, 7100
+    print(backwards_prime(start, stop))
 
 
 if __name__ == '__main__':
