18eef2473726be22d04d0a15c3b30ed1a9e3eff2
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-05-06 23:13:22 +0200

    Add binaries.py

--- binaries.py
+++ binaries.py
@@ -43,57 +43,103 @@ decode("1010111111011011011111001100") -> "0011121314"
 """
 
 
-def code(strng):
-    pass
-    
+# def code(strng):
+#     result = []
+#     for c in strng:
+#         c_bin = bin(int(c))[2:]
+#         # print(i_bin)
+#         b = (len(c_bin) - 1) * '0' + '1'
+#         # print(b)
+#         b_c = b + c_bin
+#         # print(b_c) 
+#         result.append(b_c)
+#     return ''.join(result)
+
 def decode(strng):
-    pass
-
-
+    n = '9876543210'
+    lst = []
+    for c in n:
+        c_bin = bin(int(c))[2:]
+        b_c = (len(c_bin) - 1) * '0' + '1' + c_bin
+        lst.append((c, b_c))
+    print(lst)
+    
+    result = []
+    length = len(strng)
+    count = 0
+    count_1 = 0
+    while count < length:
+        for num, num_bin in lst:
+            if num_bin in strng[count : count_1]:
+                print(num_bin, strng[count: count_1])
+                result.append(num)
+                # count = len(num_bin)
+                count = count_1
+                # count_1 += 1
+                # break
+            
+            count_1 += 1
+    return result
 
 
 
 
 def main():
-	Output: "0011100110"
-	strng = "62"
-	print(code(strng))
 
-	Output: "07"
-	strng = "10001111"
-	print(decode(strng))
-
-
-@test.describe('Tests')
-        
-def fixed_tests():
+    # # # code Output: "110110011100110000110100111000111100011000"
+    # strng = "0123456789"
+    # print(code(strng))
     
-    def testing_code(s, expected):
-        actual = code(s)
-        Test.assert_equals(actual, expected)
-    def testing_decode(s, expected):
-        actual = decode(s)
-        Test.assert_equals(actual, expected)
+    # # code Output: "11101111110110110111"
+    # strng = "10111213"
+    # print(code(strng))
+    # print('11101111110110110111')
+
+    # # code Output: "0011100110"
+    # strng = "62"
+    # print(code(strng))
+    # print('0011100110')
+
+    # decode Output: "07"
+    strng = "10001111"
+    print(decode(strng))
+    # print('10001111')
+
+    # # decode Output: "444666333666777444"
+    # strng = "001100001100001100001110001110001110011101110111001110001110001110001111001111001111001100001100001100"
+    # print(decode(strng))
+    # print('001100001100001100001110001110001110011101110111001110001110001110001111001111001111001100001100001100')
+
+# @test.describe('Tests')
         
-    @test.it('Basic Test Cases Code')
-    def basic_tests_code():
-        testing_code("62", "0011100110")
-        testing_code("55337700", "001101001101011101110011110011111010")
-        testing_code("1119441933000055", "1111110001100100110000110011000110010111011110101010001101001101")
-        testing_code("69", "00111000011001")
-        testing_code("86", "00011000001110")
+# def fixed_tests():
     
-    @test.it('Basic Test Cases Decode')
-    def basic_tests_decode():
-        testing_decode("10001111", "07")
-        testing_decode("001100001100001100001110001110001110011101110111001110001110001110001111001111001111001100001100001100", "444666333666777444")
-        testing_decode("01110111110001100100011000000110000011110011110111011100110000110001100110", "33198877334422")
-        testing_decode("0011010011010011011010101111110011000011000011000011100011100011100011100011100011100001100100011001000110011100011001001111001111001111001111001111001111", "55500011144466666699919777777")
-        testing_decode("01110111011111000110010011110011110011110011110011110011110111011101110110011001100110011001101111111010101100011001000110000001100000011000", "3331977777733322222211100019888")
+#     def testing_code(s, expected):
+#         actual = code(s)
+#         Test.assert_equals(actual, expected)
+#     def testing_decode(s, expected):
+#         actual = decode(s)
+#         Test.assert_equals(actual, expected)
+        
+#     @test.it('Basic Test Cases Code')
+#     def basic_tests_code():
+#         testing_code("62", "0011100110")
+#         testing_code("55337700", "001101001101011101110011110011111010")
+#         testing_code("1119441933000055", "1111110001100100110000110011000110010111011110101010001101001101")
+#         testing_code("69", "00111000011001")
+#         testing_code("86", "00011000001110")
+    
+#     @test.it('Basic Test Cases Decode')
+#     def basic_tests_decode():
+#         testing_decode("10001111", "07")
+#         testing_decode("001100001100001100001110001110001110011101110111001110001110001110001111001111001111001100001100001100", "444666333666777444")
+#         testing_decode("01110111110001100100011000000110000011110011110111011100110000110001100110", "33198877334422")
+#         testing_decode("0011010011010011011010101111110011000011000011000011100011100011100011100011100011100001100100011001000110011100011001001111001111001111001111001111001111", "55500011144466666699919777777")
+#         testing_decode("01110111011111000110010011110011110011110011110011110011110111011101110110011001100110011001101111111010101100011001000110000001100000011000", "3331977777733322222211100019888")
         
         
 if __name__ == '__main__':
-	main()
+    main()
 
 
 
