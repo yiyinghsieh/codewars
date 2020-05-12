abf98963a19e61281b007e0b8012191eb912cb80
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-05-12 23:42:15 +0200

    AddSolution1

--- lc0007_two_sum.py
+++ lc0007_two_sum.py
@@ -75,11 +75,20 @@ class Solution(object):
 
 class Solution(object):
     def twoSum(self, nums, target):
-    n = len(nums)
+        n = len(nums)
         for i in range(n - 1):
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+class Solution1(object):
+    def twoSum(self, nums, target):
+        dic = {}
+        for i, c in enumerate(nums):
+            if c in dic:
+                return [dic[c], i]
+            dic[target - c] = i
 
 
 def main():
@@ -106,12 +115,12 @@ def main():
     # Output: [3, 8]
     nums = [3, 2, 4, 6, 7, 4, 3, 6, 9, 9]
     target = 15
-    print(Solution().twoSum(nums, target))
+    print(Solution1().twoSum(nums, target))
 
     # Output: [28, 45]
     nums = [230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789]
     target = 542
-    print(Solution().twoSum(nums, target))
+    print(Solution1().twoSum(nums, target))
 
 
 if __name__ == '__main__':
