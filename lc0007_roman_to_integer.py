7e0a9ac9cfe69115763e8135c9ccd2c94590482f
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-05-19 23:45:29 +0200

    Add lc0007_roman_to_integer.py

---
+++ lc0007_roman_to_integer.py
@@ -0,0 +1,141 @@
+"""Leetcode 7. Roman to Integer
+Easy
+
+URL: https://leetcode.com/problems/roman-to-integer/
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C,
+D and M.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, two is written as II in Roman numeral, just two one's added 
+together. Twelve is written as, XII, which is simply X + II. The number 
+twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to 
+be within the range from 1 to 3999.
+
+Example 1:
+Input: "III"
+Output: 3
+
+Example 2:
+Input: "IV"
+Output: 4
+
+Example 3:
+Input: "IX"
+Output: 9
+
+Example 4:
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+"""
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {"I": 1, "V": 5, "X" :10, "L": 50, "C" :100,
+            "D": 500, "M": 1000}
+        # print(dic)
+        num = 0
+        subtract = 0
+        l = len(s)
+        for i in range(l - 1):
+        #     print(dic[s[i]])
+        #     num += dic[s[i]]
+        # print(num)
+        
+            if dic[s[i]] < dic[s[i + 1]]:
+                subtract += dic[s[i]]
+                # subtract += dic[s[l - 1]]
+            else:
+                num += dic[s[i]]
+        # subtract += dic[s[l - 1]]
+        num += dic[s[l - 1]]
+                
+        print(subtract, num)
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        # l = len(s)
+        # lst = []
+        # for i in range(l - 1):
+        #     if s[i] < s[i + 1]:
+        #         # break
+        #         lst.append(s[i])
+        # print(lst)
+        # num = 0
+        # for ii in lst:
+        #     # num += int(ii)
+        #     print(ii)
+        # # print(num)
+
+        # print(L)
+
+def main():
+    # # Output: 3
+    # s = "III"
+    # print(Solution().romanToInt(s))
+
+    # # Output: 4
+    # s = "IV"
+    # print(Solution().romanToInt(s))
+
+    # # Output: 9
+    # s = "IX"
+    # print(Solution().romanToInt(s))
+
+    # Output: 58
+    s = "LVIII"
+    print(Solution().romanToInt(s))
+
+    # # Output: 1994
+    # s = "MCMXCIV"
+    # print(Solution().romanToInt(s))
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
