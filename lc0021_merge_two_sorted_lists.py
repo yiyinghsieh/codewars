26879343d3d83ca431946f8a92def2bc8672f807
Yiying Hsieh <yiying.hsieh6@gmail.com>
2020-06-22 15:49:01 +0800

    Complete lc0021_merge_two_sorted_lists.py

--- lc0021_merge_two_sorted_lists.py
+++ lc0021_merge_two_sorted_lists.py
@@ -26,17 +26,47 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        print(l1.val, l1.next.val, l1.next.next.val)
+        # print(l1.val, l1.next.val, l1.next.next.val)
+        # lst = []
+        # lst.append([l1.val,l1.next.next.val,l1.next.val])
+        # print(lst)
+
+        if not l1 or not l2:
+            return l1 or l2
+
+        # Create pre_head->node1->node2....
+        pre_head = ListNode()
+        current = pre_head
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        current.next = l1 or l2
+
+        return pre_head.next
 
 
-
-
-
-
+def show(ls):
+    result = []
+    current = ls
+    while current:
+        result.append(current.val)
+        current = current.next
+    print(result)
 
 
 def main():
-    # Output: 1->1->2->3->4->4
+    # l1 = ListNode(): val=0 (default), next=None (default)
+    # l1 = ListNode(val=1): val=1, next=None (default)
+    # l1 = ListNode(val=1, next=ListNode(val=2, next=ListNode(val=4)))
+
+    # Output: [1]->1->2->3->4->4
     # l1: 1->2->4, l2: 1->3->4
     l1 = ListNode(val=1)
     l1.next = ListNode(val=2)
@@ -44,7 +74,12 @@ def main():
     l2 = ListNode(val=1)
     l2.next = ListNode(val=3)
     l2.next.next = ListNode(val=4)
-    print(Solution().mergeTwoLists(l1, l2))
+    
+    ls = Solution().mergeTwoLists(l1, l2)
+    # print(ls.val, ls.next.val, ls.next.next.val,
+    #       ls.next.next.next.val, ls.next.next.next.next.val, 
+    #       ls.next.next.next.next.next.val)
+    show(ls)
 
 
 if __name__ == '__main__':
